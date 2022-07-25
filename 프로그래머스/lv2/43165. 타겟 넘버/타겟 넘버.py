344614ee75d494dcd8224987746435a62d9dd7c1
Wonjun You <59856002+wonjunYou@users.noreply.github.com>
2022-07-25 13:45:40 +0900

    [level 2] Title: 타겟 넘버, Time: 2.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,7 +2,9 @@ def dfs(nums, i, n, t):
     ret = 0
     if i==len(nums):
         if n==t: return 1
+    
         else: return 0
+    
     ret += dfs(nums, i+1, n+nums[i], t)
     ret += dfs(nums, i+1, n-nums[i], t)
     return ret
