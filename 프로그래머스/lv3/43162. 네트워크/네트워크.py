0c9649940c30265dac97618093dc9aeb8a65aad4
Wonjun You <59856002+wonjunYou@users.noreply.github.com>
2022-07-25 15:02:12 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,21 +1,21 @@
-visited = [0 for _ in range(200)]
-
-def dfs(cur, n, computers):
-    global visited
+def dfs(visited, cur, n, computers):
     for i in range(0, n):
         if computers[cur][i] == 1 and not visited[i]:
             visited[i] = 1
-            dfs(i, n, computers)
+            dfs(visited, i, n, computers)
     return
 
 def solution(n, computers):
-    global visited
+    visited = [0 for _ in range(n)]
     answer = 0
     
-    for i in range(0, n):
+    for i in range(n):
         if not visited[i]:
             visited[i] = 1
             answer += 1
-            dfs(i, n, computers)
+            dfs(visited, i, n, computers)
+            
+        if 0 not in set(visited):
+            break
     
     return answer
